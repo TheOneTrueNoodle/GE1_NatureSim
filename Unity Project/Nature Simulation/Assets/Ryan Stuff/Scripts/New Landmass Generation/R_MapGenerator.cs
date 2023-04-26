a9552dad162c9604b215e4aed5cac6c287240180
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2023-04-26 14:57:20 +0100

    DOPE TERRAIN GENERATION!!!!!!

--- Unity Project/Nature Simulation/Assets/Ryan Stuff/Scripts/New Landmass Generation/R_MapGenerator.cs
+++ Unity Project/Nature Simulation/Assets/Ryan Stuff/Scripts/New Landmass Generation/R_MapGenerator.cs
@@ -4,18 +4,66 @@ using UnityEngine;
 
 public class R_MapGenerator : MonoBehaviour
 {
+    public enum DrawMode { NoiseMap, ColorMap, Mesh};
+    public DrawMode drawMode;
+
     [Header("Map Settings")]
-    public int mapWidth;
-    public int mapHeight;
+    const int mapChunkSize = 241;
+    [Range(0,6)] public int levelOfDetail;
     public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)] public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
 
     public bool autoUpdate;
 
+    [Header("Regions")]
+    public TerrainType[] regions;  
+
     public void GenerateMap()
     {
-        float[,] noiseMap = R_Landmass_Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = R_LandmassNoise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+
+        for (int y = 0; y < mapChunkSize; y++)
+        {
+            for (int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
 
         R_MapDisplay display = FindObjectOfType<R_MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if(drawMode == DrawMode.NoiseMap) { display.DrawTexture(R_TextureGenerator.TextureFromheightMap(noiseMap)); }
+        else if(drawMode == DrawMode.ColorMap) { display.DrawTexture(R_TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize)); }
+        else if(drawMode == DrawMode.Mesh) { display.DrawMesh(R_MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), R_TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize)); }
+    }  
+    private void OnValidate()
+    {
+        if(lacunarity < 1) { lacunarity = 1; }
+        if(octaves < 0) { octaves = 1; }
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
 }
